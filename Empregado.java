55f3ec3f7982e1e8dbbcc56b9bf782ef1158e63e
julio <julio.passsafaro@hotmail.com>
2021-09-29 19:52:39 -0300

    Primeiro Teste Realizado com Sucesso

--- Empregado.java
+++ Empregado.java
@@ -1,4 +1,56 @@
-public class Empregado{
+public class Empregado {
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
 
+    public void setTipo(int tipo){
+        this.tipo = tipo;
+    }
     
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+    
+    public void setComissao(double comissao){
+        this.comissao = comissao;
+    }
+
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+
+    public double calculaSalario(){
+        if (tipo == 1){
+            return salario;
+        }
+        else if (tipo == 2){
+            salario  = salario + salario * comissao;
+            return salario;
+        }
+        else if (tipo == 3){
+            salario = salario + bonus;
+            return salario;
+        }
+        return 0;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+    
+    public String getNome(){
+        return this.nome;
+    }
+    
+    public void setIdade (int idade){
+        this.idade = idade;
+    }
+
+    public int getIdade (){
+        return this.idade;
+    }
+
 }
